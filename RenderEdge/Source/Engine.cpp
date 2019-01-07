2589301af08f0a19e05020b63a24275e7130bad8
Алексей <en.aleksey@gmail.com>
2019-01-07 20:10:00 +0500

    update version strings

--- RenderEdge/Source/Engine.cpp
+++ RenderEdge/Source/Engine.cpp
@@ -34,7 +34,7 @@
 
 CEngine* Engine = nullptr;
 
-const std::string RenderEdgeVersion = "RenderEdge v0.3.1";
+const std::string RenderEdgeVersion = "RenderEdge v0.3.1dev";
 std::string RenderEdgePath;
 
 std::chrono::high_resolution_clock::time_point start_time, last_present_time;
